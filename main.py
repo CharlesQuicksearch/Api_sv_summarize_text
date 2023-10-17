53145c211a9bbd3261704c5cbd3370a1633e8995
CharlesQuicksearch <charles.sandholm@quicksearch.se>
2023-10-17 09:22:45 +0200

    Added logger and logs

--- main.py
+++ main.py
@@ -1,15 +1,38 @@
 from fastapi import FastAPI, HTTPException
-
+from request_and_response import Request, Response
+from procedure import summarize_text
+from Logger_Config import logger
+from Logger_Config.logger import logging
+import uvicorn
+import json
 
 app = FastAPI()
+logger.config_logger()
+
 
 @app.get("/home")
 def home():
     return "Summarize swedish text API. Send application/json: 'input':'your article' "
 
-@app.post("/summarize/")
-def summarize():
+
+@app.post("/summarize/", response_model=Response)
+def summarize(request_data: Request):
     try:
-        return None
+        logging.info("Summarize called. Trying to run AI procedure.")
+        result = summarize_text(request_data.input)
+        logging.info(f"Summarize finished. Returning: {result}")
+        return Response(output=result)
     except Exception as e:
-       raise HTTPException(status_code=500, detail=str(e))
+        error = HTTPException(status_code=500, detail=str(e))
+        logging.warning(f"{error}")
+        raise error
+
+
+if __name__ == "__main__":
+
+    logging.info("Application start up.")
+
+    with open("config.json", "r") as f:
+        config = json.load(f)
+    logging.info(f"Hosting API on {config.get('host')}:{config.get('port')}")
+    uvicorn.run(app, host=config.get("host"), port=int(config.get("port")))
--- procedure.py
+++ procedure.py
@@ -1,11 +1,15 @@
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+from Logger_Config.logger import logging
 
 model = AutoModelForSeq2SeqLM.from_pretrained('model')
 tokenizer = AutoTokenizer.from_pretrained('model')
 
-def summarize_text(input):
-    tokens = tokenizer(input, return_tensors="pt").input_ids
+
+def summarize_text(text):
+    logging.info("Start summarize text.")
+    tokens = tokenizer(text, return_tensors="pt").input_ids
     outputs = model.generate(input_ids=tokens, max_length=130, num_beams=5, num_return_sequences=1)
     result = tokenizer.batch_decode(outputs, skip_special_tokens=True)
+    logging.info(f"Summarized text: {result[0]}")
 
-    return result
+    return result[0]
