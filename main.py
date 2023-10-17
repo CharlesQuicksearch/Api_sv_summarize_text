67c1074e11d77d1a13e466b5f7338344eedd9095
CharlesQuicksearch <charles.sandholm@quicksearch.se>
2023-10-17 09:50:34 +0200

    minor changes

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def summarize(request_data: Request):
     try:
         logging.info("Summarize called. Trying to run AI procedure.")
         result = summarize_text(request_data.input)
-        logging.info(f"Summarize finished. Returning: {result}")
+        logging.info(f"Returning result.")
         return Response(output=result)
     except Exception as e:
         error = HTTPException(status_code=500, detail=str(e))
@@ -34,5 +34,9 @@ if __name__ == "__main__":
 
     with open("config.json", "r") as f:
         config = json.load(f)
-    logging.info(f"Hosting API on {config.get('host')}:{config.get('port')}")
-    uvicorn.run(app, host=config.get("host"), port=int(config.get("port")))
+
+    host = config.get('host')
+    port = config.get("port")
+
+    logging.info(f"Hosting API on {host}:{port}")
+    uvicorn.run(app, host=host, port=int(port))
